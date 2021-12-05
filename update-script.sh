73efd8bc650c0d0b7ca2f76dec8853b4a930d693
Joseph Vossler <joseph.vossler1@gmail.com>
2021-12-05 14:35:51 -0500

    Updates to update-script

--- update-script.sh
+++ update-script.sh
@@ -10,24 +10,54 @@ list_of_commands=(
 )
 
 system_update(){
-	for CMD in "${list_of_commands[@]}"
-	do
-		$CMD
-		if
-			[ $? -eq 0 ]
-		then
-			echo "$CMD was successfully run, exit status code: $?"
-			echo ""
-		elif
-			[ $? -ne 0 ]
-		then
-			echo "Error running $CMD on $(date), exit status code: $?" >> /home/j/scripts/update-log.txt
-			continue
-		fi
-		echo "Exit status code: $?"
-	done	
+for CMD in "${list_of_commands[@]}"
+do
+	echo "---"
+	echo "Running $CMD"
+	echo ""
+	$CMD
+
+	if
+		[ $? -eq 0 ]
+	then
+		echo "$CMD was successfully run, exit status code: $?"
+		echo ""
+
+#	if
+#		[ $? -eq 0 ]
+#	then
+##		echo "System successfully updated on $(date)" >> /home/j/scripts/update-log.txt
+#		echo "$CMD was successfully run"
+#	else
+#		if
+#			[ $? -ne 0]
+#		then
+#			echo "Error running system update command $CMD on $(date)" >> /home/j/scripts/update-log.txt
+#		fi
+#	fi
+	fi
+done	
 }
 
-system_update
 
+system_update
+echo "Exit status code:  $?"
+
+if [ $? -eq 0 ]
+then
+	echo "System successfully updated on $(date)" >> /home/j/scripts/update-log.txt
+elif
+	[ $? -ne 0 ]
+then
+	echo "Error running $CMD on $(date), exit status code: $?" >> /home/j/scripts/update-log.txt
+fi
+
+#if [ $? -ne 0 ]
+#then
+#	echo "Error running $CMD on $(date)" >> /home/j/scripts/update-log.txt
+#elif 
+#	[ $? -eq 0 ]
+#then
+#		echo "System successfully updated on $(date)" >> /home/j/scripts/update-log.txt
+#fi
 
